aa83e56662933be960074117ee4b92efe7d78f67
Aurélie PREAUD <aureliepreaud@MacBook-Air-de-Aurelie.local>
2022-12-28 19:27:17 +0100

    heroku to vercel v1

--- index.js
+++ index.js
@@ -451,16 +451,9 @@ app.get("/", (req, res) => {
   });
 });
 
-// Heroku va nous fournir une variable process.env.PORT
-// if (process.env.PORT) {
-//   app.listen(process.env.PORT, () => {
-//     console.log("Server started");
-//   });
-// } else {
-//   app.listen(3200, () => {
-//     console.log("Server started");
-//   });
-// }
+app.all("*", (req, res) => {
+  res.json("This endpoint does not exist.");
+});
 
 app.listen(process.env.PORT || 3200, () => {
   console.log("Server started");
